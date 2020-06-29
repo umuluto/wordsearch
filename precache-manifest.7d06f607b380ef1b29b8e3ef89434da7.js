0646115e7e70793eedd33a4ce38e5bfe6a998df4
umuluto <ulto.mn@gmail.com>
2020-06-30 03:45:25 +0700

    Updates

---
+++ precache-manifest.7d06f607b380ef1b29b8e3ef89434da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f64c2f54197fde52eeb73d3ce9db04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c78630672fbdd73efb",
+    "url": "/static/css/main.4a78cf43.chunk.css"
+  },
+  {
+    "revision": "ebdfa7f2eb5697a651f8",
+    "url": "/static/js/2.5eb11dad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5eb11dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c78630672fbdd73efb",
+    "url": "/static/js/main.bb255882.chunk.js"
+  },
+  {
+    "revision": "27d8b743222c116f4b05",
+    "url": "/static/js/runtime-main.bc8a4512.js"
+  }
+]);
