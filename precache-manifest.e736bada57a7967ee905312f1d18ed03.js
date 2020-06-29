e1aa0431800801c9bdab9a647f7ce672693f2b23
umuluto <ulto.mn@gmail.com>
2020-06-30 03:56:43 +0700

    Updates

---
+++ precache-manifest.e736bada57a7967ee905312f1d18ed03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499935f5cb3d8ae67840afec281eec06",
+    "url": "/wordsearch/index.html"
+  },
+  {
+    "revision": "08f832b42c376d72e95b",
+    "url": "/wordsearch/static/css/main.4a78cf43.chunk.css"
+  },
+  {
+    "revision": "ebdfa7f2eb5697a651f8",
+    "url": "/wordsearch/static/js/2.5eb11dad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wordsearch/static/js/2.5eb11dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f832b42c376d72e95b",
+    "url": "/wordsearch/static/js/main.de87939e.chunk.js"
+  },
+  {
+    "revision": "ef30218661624a878faf",
+    "url": "/wordsearch/static/js/runtime-main.c8a04ef0.js"
+  }
+]);
